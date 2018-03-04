eedfab0ccedd1c474d31e5cbe50cd1de227f4d5d
TimHaa <36132493+TimHaa@users.noreply.github.com>
2018-03-04 14:33:20 +0100

    Added rnd array generator + function to find quadrants containing nr.

--- SudokuApp/Program.cs
+++ SudokuApp/Program.cs
@@ -6,7 +6,55 @@ namespace SudokuApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SudokuSolver s = new SudokuSolver();
+
+            int[,,] sud = new int[9, 9, 9];
+
+            //generate random numbers
+            Random rnd = new Random();
+            for (int m = 0; m <= 8; m++)
+            {
+                for (int n = 0; n <= 8; n++)
+                {
+                    sud[n, m, 0] = rnd.Next(1, 10);
+                }
+            }
+
+            //print sudoku
+            for (int i = 0; i < 9; i++)
+            {
+                Console.Write("| ");
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(sud[j, i, 0] + " ");
+
+                    if (j % 3 == 2) { Console.Write("| "); }
+
+                }
+                Console.Write("\n");
+                if (i % 3 == 2)
+                {
+                    Console.WriteLine("_________________________");
+                    Console.WriteLine();
+                }
+
+
+            }
+
+            s.SolveLogically(sud);  //function: shows which number appears in which quadrant(1 - 9)
+
+            //Console.Write("\n");
+            //for (int i = 0; i < 9; i++)
+            //{
+            //    for (int j = 0; j < 9; j++)
+            //    {
+            //        Console.Write(sud[i, j, 8]+ " ");
+            //    }
+            //    Console.Write("\n");
+
+            //}
+
+            Console.ReadLine()
         }
     }
 }
---
+++ SudokuApp/SudokuSolver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuApp
+{
+    class SudokuSolver
+    {
+        public SudokuSolver() { }
+
+        public void SolveLogically(int[,,] sudokuInput)
+        {
+            bool containsI;
+            for (int i = 1; i <= 9; i++)
+            {
+                Console.Write(i + ":");
+                for (int j = 0; j < 9; j++)
+                {
+                    containsI = false;
+                    for (int k = 0; k < 9; k++)
+                    {
+                        if (sudokuInput[j % 3 * 3 + k % 3, j / 3 * 3 + k / 3, 0] == i) { containsI = true; break; }
+                    }
+                    if (containsI)
+                    {
+                        Console.Write(j + 1 + ",");
+
+                        //check zeilen + spalten -> wenn sie enthalten i * zehn in iter dim speichern in verlängerung der spalte
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+        public void SolveTest(int[,,] input)
+        {
+
+        }
+    }
+}
