f5c5882afca6db0b8a6e548f318e7071a24e6a5f
TimHaa <36132493+TimHaa@users.noreply.github.com>
2018-03-05 18:02:05 +0100

    cleanup + ToDo's

--- SudokuApp/SudokuSolver.cs
+++ SudokuApp/SudokuSolver.cs
@@ -10,42 +10,44 @@ namespace SudokuApp
 
         public void SolveLogically(int[,,] sudokuInput)
         {
+            //TODO add generatorfunction (saving numbers in respective layer for each entry) and solved condition
             int nrsFound = 1;
             while (nrsFound > 0)//TODO change whilecondition to not solved
             {
                 Console.WriteLine("whileLoop starts");
                 nrsFound = 0;
-                bool containsI;
+                //foreach number 1 to 9 iterate through the 9 quadrants and check if they contain the number
                 for (int currentNr = 1; currentNr <= 9; currentNr++)
                 {
-
-                    for (int j = 0; j < 9; j++)
+                    bool containsCurrNr;
+                    for (int quadrant = 0; quadrant <= 8; quadrant++)
                     {
-                        containsI = false;
-                        for (int k = 0; k < 9; k++)
+                        containsCurrNr = false;
+                        int quadStartCol = quadrant % 3 * 3;
+                        int quadStartRow = quadrant / 3 * 3;
+                        for (int iterateQuad = 0; iterateQuad < 9; iterateQuad++)
                         {
-                            if (sudokuInput[j % 3 * 3 + k % 3, j / 3 * 3 + k / 3, 0] == currentNr) { containsI = true; break; }
+                            if (sudokuInput[quadStartCol + iterateQuad % 3, quadStartRow + iterateQuad / 3, 0] == currentNr) { containsCurrNr = true; break; }
                         }
-                        if (!containsI)
+                        //If not containing save information about possible position in the layer of said number
+                        if (!containsCurrNr)
                         {
-                            //Containfunction:
-                            //Console.WriteLine("quadrant " + (j + 1) + " does not contain " + i + ".");
-
                             for (int threeIndex = 0; threeIndex < 3; threeIndex++)
                             {
                                 for (int nineIndex = 0; nineIndex < 9; nineIndex++)
                                 {
-                                    if (sudokuInput[nineIndex, j / 3 * 3 + threeIndex, 0] == currentNr)
+                                    if (sudokuInput[nineIndex, quadStartRow + threeIndex, 0] == currentNr)//TODO maybe optimize so it doesnt consider the current quadrant
                                     {
-                                        for (int m = 0; m < 3; m++) { sudokuInput[j % 3 * 3 + m, j / 3 * 3 + threeIndex, currentNr] = currentNr; }
+                                        for (int m = 0; m < 3; m++) { sudokuInput[quadStartCol + m, quadStartRow + threeIndex, currentNr] = currentNr; }
                                     }
-                                    if (sudokuInput[j % 3 * 3 + threeIndex, nineIndex, 0] == currentNr)
+                                    if (sudokuInput[quadStartCol + threeIndex, nineIndex, 0] == currentNr)
                                     {
-                                        for (int n = 0; n < 3; n++) { sudokuInput[j % 3 * 3 + threeIndex, j / 3 * 3 + n, currentNr] = currentNr; }
+                                        for (int n = 0; n < 3; n++) { sudokuInput[quadStartCol + threeIndex, quadStartRow + n, currentNr] = currentNr; }
                                     }
                                 }
-
                             }
+                            //check this numbers layer for single zeros in quadrants, these represent solutions
+                            //Also store info using rows/cols of zeros to find solutions in next run
                             int zeroCount = 0;
                             int colOfZero = 0;
                             int rowOfZero = 0;
@@ -53,21 +55,23 @@ namespace SudokuApp
                             List<int> checkSameCol = new List<int>();
                             for (int l = 0; l < 9; l++)
                             {
-                                if (sudokuInput[j % 3 * 3 + l % 3, j / 3 * 3 + l / 3, currentNr] == 0)
+                                if (sudokuInput[quadStartCol + l % 3, quadStartRow + l / 3, currentNr] == 0)
                                 {
                                     zeroCount++;
-                                    colOfZero = j % 3 * 3 + l % 3;
-                                    rowOfZero = j / 3 * 3 + l / 3;
+                                    colOfZero = quadStartCol + l % 3;
+                                    rowOfZero = quadStartRow + l / 3;
                                     checkSameCol.Add(colOfZero);
                                     checkSameRow.Add(rowOfZero);
                                 }
                             }
-                            if (zeroCount == 1)
+                            if (zeroCount == 1)//TODO whenever a number is found, delete the space from all other layers
                             {
-                                sudokuInput[colOfZero, rowOfZero, 0] = currentNr; sudokuInput[colOfZero, rowOfZero, currentNr] = currentNr; nrsFound++;
+                                sudokuInput[colOfZero, rowOfZero, 0] = currentNr;
+                                sudokuInput[colOfZero, rowOfZero, currentNr] = currentNr;
+                                nrsFound++;
                                 Console.WriteLine("col: " + (colOfZero + 1) + " / row: " + (rowOfZero + 1));
                             }
-                            else if (zeroCount > 1)
+                            else if (zeroCount > 1)//TODO not tested yet
                             {
                                 bool sameCol = true;
                                 foreach (int possibleCol in checkSameCol) { if (possibleCol != colOfZero) { sameCol = false; } }
@@ -77,21 +81,19 @@ namespace SudokuApp
                                 {
                                     for (int n = 0; n < 9; n++)
                                     {
-                                        if (n < (j / 3 * 3) || n > (j / 3 * 3 + 2)) { sudokuInput[colOfZero, n, currentNr] = currentNr; }
+                                        if (n < quadStartRow || n > (quadStartRow + 2)) { sudokuInput[colOfZero, n, currentNr] = currentNr; }
                                     }
                                 }
                                 if (sameRow)
                                 {
                                     for (int n = 0; n < 9; n++)
                                     {
-                                        if (n < (j % 3 * 3) || n > (j % 3 * 3 + 2)) { sudokuInput[n, rowOfZero, currentNr] = currentNr; }
+                                        if (n < quadStartCol || n > (quadStartCol + 2)) { sudokuInput[n, rowOfZero, currentNr] = currentNr; }
                                     }
                                 }
                             }
                         }
-
                     }
-                    
                 }
                 Print(sudokuInput, 0);
             }
@@ -115,6 +117,7 @@ namespace SudokuApp
                 }
             }
         }
+
         public void SolveTest(int[,,] input)
         {
 
